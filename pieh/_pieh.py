c2dbb7573e123f2d45d974c60989f896d39da172
niris <smresc@gmail.com>
2012-10-11 22:01:25 +0800

    restart

---
+++ pieh/_pieh.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main():
+    # TODO
+    # 查找本地文件
+    # 查找记录文件
+    # 对比文件记录
+
+    # 新文件/修改文件 新建
+    # 旧文件 不变
+    # 丢失文件 删除
+
+    # 生成html
+
+    print('test');
+
+if __name__ == '__main__':
+    main()
