0f22e4c7987124d54ce4ec595c81a6f7e7729bae
niris <smresc@gmail.com>
2012-10-14 11:36:08 +0800

    add note android

--- _pieh/_.py
+++ _pieh/_.py
@@ -7,6 +7,7 @@ import sqlite3
 import filecmp
 import hashlib
 
+import markdown2
 import config
 
 # TODO
@@ -17,37 +18,12 @@ import config
 # 生成html
 
 class Application():
-    def _parser_filename(self, filename):
-        name = filename.rsplit('.', 1)[0]
-        return tuple(name.split('_', 1))
+    def main(self):
+        self.initial()
+        self.generate_posts()
 
-    def _check_status(self, source):
-        """return
-        1 => equal
-        0 => not equal
-        -1 => file not found
-        """
-        date, title = self._parser_filename(source)
-        post_file = os.path.join(config.path['post'], date, title)
-        source_file = os.path.join(config.path['source'], source)
-        if os.path.exists(post_file):
-            if filecmp.cmp(source_file, post_file):
-                return 1
-            else:
-                return 0
-        else:
-            return -1
-
-    def _update_post(self, source):
-        pass
-
-    def _generate_post(self, source):
-        pass
-
-    def _generate_page(self, page):
-        pass
-
-    def generate_pages(self):
+    def initial(self):
+        """创建各种文件夹"""
         pass
 
     def generate_posts(self):
@@ -59,10 +35,55 @@ class Application():
             elif status == 0:
                 self._update_post(source)
             else:
-                self._generate_post(source)
+                self._insert_post(source)
 
-    def main(self):
-        self.generate_posts()
+    def _insert_post(self, filename):
+        date, title = self._parser_filename(filename)
+        directory = os.path.join(config.path['post'], date)
+        if not os.exists(directory):
+            os.mkdir(directory)
+        source_file = self._source_path(filename)
+        post_file = self._post_path(filename, date, title)
+        with open(post_file, 'w') as post:
+            post.write(markdown2.markdown_path(source_file))
+        # TODO
+        # 数据库存储信息
+
+    def _update_post(self, source): pass
+    def _delete_post(self, source): pass
+
+    def _source_path(self, filename):
+        return os.path.join(config.path['source'], filename)
+
+    def _post_path(self, filename, date=None, title=None):
+        if not (date and title):
+            date, title = self._parser_filename(filename)
+        return os.path.join(config.path['post'], date, title)
+
+    def _parser_filename(self, filename):
+        """return (date, title)"""
+        name = filename.rsplit('.', 1)[0]
+        return tuple(name.split('_', 1))
+
+
+    def _check_status(self, filename):
+        """return
+        1 => equal
+        0 => not equal
+        -1 => post not found
+        """
+        source_file = self._source_path(filename)
+        post_file = self._post_path(filename)
+        if os.path.exists(post_file):
+            if filecmp.cmp(source_file, post_file):
+                return 1
+            else:
+                return 0
+        else:
+            return -1
+
+    def _new_page(self, page): pass
+    def generate_pages(self): pass
 
 
 class Database():
@@ -98,8 +119,8 @@ class Database():
                 sha TEXT NOT NULL,
                 -- 文章的唯一标识
                 key TEXT NOT NULL,
-                -- 源文件 目标文件 标题 作者 发布时间 更新时间 分类 标签
-                -- source post title author publish update callable tag
+                -- 源文件 目标文件 标题 作者分类 标签 发布时间 更新时间
+                -- source post title author category tag publish update
                 value TEXT NOT NULL
                 -- 值
             );
