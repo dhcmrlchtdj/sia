d96ea2df9e7a65074bd33f88ec21efeb96e66901
h11 <nirisix@gmail.com>
2019-01-20 05:52:54 +0800

    fix: remove offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
             },
         },
         `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-offline`,
+        `gatsby-plugin-remove-serviceworker`,
         `gatsby-plugin-feed`,
     ],
 };
